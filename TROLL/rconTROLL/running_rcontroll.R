9742193df671041384ab213f70921657951a8d68
BiancaRius <biancafaziorius@gmail.com>
2024-11-28 15:39:50 +0100

    updating gitignore and adding some files to be tracked

--- TROLL/rconTROLL/running_rcontroll.R
+++ TROLL/rconTROLL/running_rcontroll.R
@@ -12,16 +12,13 @@ troll(
   path = "/Users/biancarius/Desktop/Postdoc_Toulouse/Postdoc_Toulouse/TROLL/rconTROLL/",
   global = generate_parameters(
     cols = 100, rows = 100,
-    iterperyear = 12, nbiter = 1200
-  ),
+    iterperyear = 12, nbiter = 1200 * 1),
   species = TROLLv3_species,
   climate = TROLLv3_climatedaytime12,
-  daily = TROLLv3_input
+  daily = TROLLv3_daytimevar
 )
-
 
 #load the outputs and create a trollsim() object
 t1 = load_output("test_outputs","/Users/biancarius/Desktop/Postdoc_Toulouse/Postdoc_Toulouse/TROLL/rconTROLL/test_outputs/")
 
-#The plot is supper weird
-autoplot(t1)
+autoplot(test, what = "temporal")
