4b60f9a382b3ef68da04f057dc3515bc8d795c56
BiancaRius <biancafaziorius@gmail.com>
2026-03-14 16:44:16 +0100

    Hydraulic locking sensitivity test: theta_thres1e-5_noveg

--- TROLL/TROLL_code_understanding_documenting/mainTROLL4.0_WTD.cpp
+++ TROLL/TROLL_code_understanding_documenting/mainTROLL4.0_WTD.cpp
@@ -8031,8 +8031,8 @@ if (_WATER_RETENTION_CURVE==1) {
                 }
 
 
-                if(theta_w_cap < 1e-6) {
-                    theta_w_cap = 1e-6; // BR - changing limit value added by SS to detect very small values but that are not precisely 0
+                if(theta_w_cap < 1e-5) {
+                    theta_w_cap = 1e-5; // BR - changing limit value added by SS to detect very small values but that are not precisely 0
                 } else if(theta_w_cap > 1.0) {
                     theta_w_cap = 1.0; // BR - changing limit value added by SS to detect very large values but that are not precisely 1
                 }
