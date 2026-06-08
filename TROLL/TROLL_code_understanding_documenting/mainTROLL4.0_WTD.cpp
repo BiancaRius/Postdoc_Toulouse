fa3b1119ba5993c60578e6a6a3ec62b12edecfb2
BiancaRius <biancafaziorius@gmail.com>
2026-06-08 18:19:35 +0200

    reducing 30% of precipitation with ksfloor 1e-14

--- TROLL/TROLL_code_understanding_documenting/mainTROLL4.0_WTD.cpp
+++ TROLL/TROLL_code_understanding_documenting/mainTROLL4.0_WTD.cpp
@@ -5458,7 +5458,8 @@ void Tree::Fluxh(int h,float &PPFD, float &VPD, float &Tmp, float &leafarea_laye
             
             tnight=NightTemperature[0];
             // precip=Rainfall[0];
-            precip=Rainfall[0]*0.5;
+            // precip=Rainfall[0]*0.5;
+            precip=Rainfall[0]*0.3;
             WSDailyMean=DailyMeanWindSpeed[0];
             WDailyMean=DailyMeanIrradiance[0]*SWtoPPFD;
             tDailyMean=DailyMeanTemperature[0];
@@ -7477,7 +7478,8 @@ if (_WATER_RETENTION_CURVE==1) {
     */            
             tnight=NightTemperature[iter%nbdays];
             // precip=Rainfall[iter%nbdays];
-            precip=Rainfall[iter%nbdays]*0.5;
+            // precip=Rainfall[iter%nbdays]*0.5;
+            precip=Rainfall[iter%nbdays]*0.3;
             WSDailyMean=DailyMeanWindSpeed[iter%nbdays];
             WDailyMean=DailyMeanIrradiance[iter%nbdays]*SWtoPPFD;
             tDailyMean=DailyMeanTemperature[iter%nbdays];
@@ -7995,8 +7997,8 @@ if (_WATER_RETENTION_CURVE==1) {
                 // INCLUDE:  Checking sanity of calculated variables for capillary rise //BR
 }
 
-                if(Ks_cap[l][d] < 1e-12) {
-                    Ks_cap[l][d] = 1e-12; // BR changing the limit to avoid ks and ks harmonic = 0 and as a consequence to hydraulic locking
+                if(Ks_cap[l][d] < 1e-14) {
+                    Ks_cap[l][d] = 1e-14; // BR changing the limit to avoid ks and ks harmonic = 0 and as a consequence to hydraulic locking
                 }
 
                 // Update soil phi
